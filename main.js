ad917a703fa815af2e56039d7f6bd4db10c653ac
Brooke Canter <brookebc@live.unc.edu>
2017-09-21 15:57:18 -0400

    Add files via upload
    Resume

---
+++ main.js
@@ -0,0 +1,24 @@
+/* DOMContentLoaded will ensure that the js is run before the html
+b/c before .....
+*/
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    var myIndex = 0;
+    carousel();
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        console.log(x)
+        for(i=0;i<x.length;i++) {
+            x[i].style.display="none";
+        }
+        myIndex++;
+        if (myIndex > x.length) {
+            myIndex = 1
+        }
+        x[myIndex-1].style.display = "block";
+        setTimeout(carousel, 6000);
+    }
+    
+ })
